3944b79a04f8c220b93ea582ab121231e8b01d5d
TetianaChernenko <113249605+TetianaChernenko@users.noreply.github.com>
2023-03-04 13:05:35 +0000

    Deploying to gh-pages from @ TetianaChernenko/goit-js-hw-09@2d107abfae6275319595aea2a4135c721a44c51e 🚀

---
+++ 01-color-switcher.85da8b9f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");var d=null;t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.85da8b9f.js.map
